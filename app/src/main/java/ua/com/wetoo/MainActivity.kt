d1b4cefb1da1d5942ad8fa674a222033831b126d
OS.Danych <alexgdns@gmail.com>
2025-08-07 07:28:28 +0300

    finished splashScreen

--- app/src/main/java/ua/com/wetoo/MainActivity.kt
+++ app/src/main/java/ua/com/wetoo/MainActivity.kt
@@ -1,27 +1,28 @@
 package ua.com.wetoo
 
 import android.os.Bundle
-import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import ua.com.wetoo.ui.theme.WeTooTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,21 +40,35 @@ fun SplashScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(100.dp),
+            .padding(80.dp)
+            .background(colorResource(id = R.color.backgroundCream)),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Hello, join us and find your community")
+        Text(text = "Welcome", fontSize = 30.sp, modifier = Modifier.padding(bottom = 20.dp))
+        Text(text = "Find your hobby class", fontSize = 18.sp)
+        Text(text = "or", fontSize = 18.sp)
+        Text(text = "create your own", fontSize = 18.sp)
+
         Image(
             painter = painterResource(id = R.drawable.ic_splash),
             contentDescription = "splash icon",
-            modifier = Modifier
-                .height(150.dp)
-                .width(150.dp),
-
-            )
-        Button(onClick = {}) {
-            Text("Register")
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(R.color.primaryOrange),
+                contentColor = colorResource(R.color.backgroundCream)
+            ),
+            shape = RoundedCornerShape(8),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Register", fontSize = 18.sp)
+        }
+        TextButton(
+            onClick = {}) {
+            Text("Sign in", fontSize = 18.sp, color = colorResource(R.color.primaryOrange))
         }
     }
 
